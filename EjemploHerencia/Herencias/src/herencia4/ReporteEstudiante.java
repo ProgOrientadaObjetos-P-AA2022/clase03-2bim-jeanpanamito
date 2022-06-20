ed12bb24ce43b5c361cc3e7c6d1a49db324da41d
jeanpanamito <jppanamito@utpl.edu.ec>
2022-06-20 11:50:22 -0500

    herencia4 terminada

--- EjemploHerencia/Herencias/src/herencia4/ReporteEstudiante.java
+++ EjemploHerencia/Herencias/src/herencia4/ReporteEstudiante.java
@@ -15,4 +15,49 @@ public class ReporteEstudiante extends Reporte {
     double promedioMatriculas;
     ArrayList<Estudiante> lista;
     
+    public ReporteEstudiante(String n) {
+        super(n);
+
+    }
+
+    public double obtenerPromedioMatriculas() {
+        return promedioMatriculas;
+    }
+
+    public void establecerPromedioMatriculas() {
+        for (int i = 0; i < lista.size(); i++) {
+            promedioMatriculas = promedioMatriculas
+                    + lista.get(i).getMatricula();
+        }
+        promedioMatriculas = promedioMatriculas/lista.size();
+    }
+
+    public ArrayList<Estudiante> obtenerListaEstudiantes() {
+        return lista;
+    }
+
+    public void establecerListaEstudiantes(ArrayList<Estudiante> lista) {
+        this.lista = lista;
+    }
+
+    @Override
+    public String toString() {
+        String cadenaEstudiantes = String.format("%s\n", "Lista Estudiante");
+        for (int i = 0; i < lista.size(); i++) {
+            cadenaEstudiantes = String.format("%s%s\n",
+                    cadenaEstudiantes,
+                    lista.get(i));
+
+        }
+
+        String cadena = String.format("\nCodigo: %s \n"
+                + "%s"
+                + "El promedio de matriculas es: %.2f\n",
+                codigo,
+                cadenaEstudiantes,
+                obtenerPromedioMatriculas());
+        return cadena;
+
+    }
+
 }
--- EjemploHerencia/Herencias/src/herencia4/ReportePolicia.java
+++ EjemploHerencia/Herencias/src/herencia4/ReportePolicia.java
@@ -15,4 +15,49 @@ import java.util.ArrayList;
 public class ReportePolicia extends Reporte{
     double promedioEdades;
     ArrayList<Policia> lista;
+     
+    
+    public ReportePolicia(String n) {
+        super(n);
+
+    }
+
+    public double obtenerPromedioEdades() {
+        return promedioEdades;
+    }
+
+    public void establecerPromedioEdades() {
+        for (int i = 0; i < lista.size(); i++) {
+            promedioEdades = promedioEdades
+                    + lista.get(i).getEdad();
+        }
+        promedioEdades = promedioEdades/lista.size();
+    }
+
+    public ArrayList<Policia> obtenerListaPolicias() {
+        return lista;
+    }
+
+    public void establecerListaPolicias(ArrayList<Policia> lista) {
+        this.lista = lista;
+    }
+
+    @Override
+    public String toString() {
+        String cadenaPolicias = String.format("%s\n", "Lista Policias");
+        for (int i = 0; i < lista.size(); i++) {
+            cadenaPolicias = String.format("%s%s\n",
+                    cadenaPolicias,
+                    lista.get(i));
+
+        }
+
+        String cadena = String.format("\nCodigo: %s \n"
+                + "%s"
+                + "El promedio de edades es: %.2f\n",
+                codigo,
+                cadenaPolicias,
+                obtenerPromedioEdades());
+        return cadena;
+    }
 }
